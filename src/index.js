fe9d8174579ab2d4171211c8fed6fa66f8eb8c64
Mayong <1345959121@qq.com>
2020-05-25 23:55:21 +0800

    react基础部分补充

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-// import App from './App'
-import Todo from './todolist'
+import App from './App'
+// import Todo from './todolist'
 
-ReactDOM.render(<Todo/>,document.getElementById('root'))
+ReactDOM.render(<App/>,document.getElementById('root'))
---
+++ src/todoItem.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import propTypes from 'prop-types'
+
+class todoItem extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+
+        }
+    }
+
+    componentWillMount() {
+        console.log('组件将要挂载')
+    }
+    componentDidMount() {
+        console.log('组件将挂载over')
+    }
+    // 这个传递父组件方法有点迷，主要是涉及到this不固定的问题。还有点迷糊
+    render() { 
+        console.log('组件渲染')
+        return ( 
+            <li onClick={this.props.delete.bind(this, this.props.index)}>
+                {this.props.content}
+                {this.props.selfIndex}
+            </li>
+         )
+    }
+}
+todoItem.propTypes = {
+    content: propTypes.string,
+    index: propTypes.number,
+    delete:propTypes.func.isRequired
+}
+
+todoItem.defaultProps = {
+    selfIndex: 'selfIndex'
+
+}
+ 
+export default todoItem;
