af512d4c7f7cb2a2dc2766fd9ae3d65c09e5d436
Mayong <1345959121@qq.com>
2020-06-01 17:11:07 +0800

    react小书分支初始化

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-// import App from './App'
-import AntdTodo from './antd-todo.js'
-// import Todo from './todolist'
-
-ReactDOM.render(<AntdTodo/>,document.getElementById('root'))
