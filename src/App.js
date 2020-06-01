e3d267f05f3ce8bd0b07125cc9a4086b6a752eb6
Mayong <1345959121@qq.com>
2020-06-01 22:47:39 +0800

    react小书初始化src

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <div>
+                app组件
+                react小书初始化
+            </div>
+        );
+    }
+}
+ 
+export default App;
