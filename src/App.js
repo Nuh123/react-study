37dff63935ae7d481388fdba4d4702e90cf06504
Mayong <1345959121@qq.com>
2020-06-18 13:14:06 +0800

    1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
+import Test from './test';
 
 class App extends Component {
     constructor(props) {
-        super(props);
-        this.state = {  }
+        super(props)
+        this.state = { 
+            content: 'test',
+            stateData: {
+                themeColor: 'red',
+                themeName: 'redTheme'
+            }
+         }
     }
     render() { 
         return ( 
-            <div>
+            <div >
+                {/* <div dangerouslySetInnerHTML={{__html:this.state.content}}> */}
                 app组件
                 react小书初始化
+                <Test them = {this.state.content}/>
             </div>
         );
     }
