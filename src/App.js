2173852384f129147450fda570cfc1d10805fbe6
Mayong <1345959121@qq.com>
2020-06-26 17:31:37 +0800

    react小书评论组件初始化

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React, { Component } from 'react';
-import Test from './test';
 
 class App extends Component {
     constructor(props) {
         super(props)
         this.state = { 
-            content: 'test',
-            stateData: {
-                themeColor: 'red',
-                themeName: 'redTheme'
-            }
+           
          }
     }
     render() { 
         return ( 
             <div >
-                {/* <div dangerouslySetInnerHTML={{__html:this.state.content}}> */}
-                app组件
-                react小书初始化
-                <Test them = {this.state.content}/>
+                react小书评论组件初始化
+                
             </div>
         );
     }
