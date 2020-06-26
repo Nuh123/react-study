b4862a60425e5f680a1a499055f0526cb21e8568
Mayong <1345959121@qq.com>
2020-06-26 18:07:30 +0800

    初始版本评论组件

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import CommentInput from './CommentInput'
+import CommentList from './CommentList'
+import './index.css'
 
 class App extends Component {
     constructor(props) {
         super(props)
         this.state = { 
-           
-         }
+            comments:[]
+        }
     }
     render() { 
         return ( 
-            <div >
-                react小书评论组件初始化
-                
+            <div className='wrapper'>
+                <CommentInput onSubmit={this.handleSubmitComment.bind(this)}/>
+                <CommentList comments={this.state.comments}/>
             </div>
         );
+    }
+    handleSubmitComment (comment) {
+        if (!comment) return
+        if (!comment.username) return alert('请输入用户名')
+        if (!comment.content) return alert('请输入评论内容')
+        this.state.comments.push(comment)
+        this.setState({
+          comments: this.state.comments
+        })
     }
 }
  
---
+++ src/CommentList.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import Comment from './Comment'
+
+class CommentList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+     
+    render() {
+        // const comments = [
+        //     {username: 'Jerry', content: 'Hello'},
+        //     {username: 'Tomy', content: 'World'},
+        //     {username: 'Lucy', content: 'Good'}
+        // ]
+    
+        return (
+            <div>
+                {this.props.comments.map((comment, i) => <Comment comment={comment} key={i} />)}
+            </div>
+        )
+    }
+
+
+}
+ 
+export default CommentList
