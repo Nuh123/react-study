c4a3a9f705eed316be6c398c3487e6e52a3571ac
Mayong <1345959121@qq.com>
2020-08-18 20:19:30 +0800

    redux介入

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import App from './containers/App'
+import commentsReducer from './reducers/comments'
 import './index.css'
 
-ReactDOM.render(<App/>,document.getElementById('root'))
+const store = createStore(commentsReducer)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
